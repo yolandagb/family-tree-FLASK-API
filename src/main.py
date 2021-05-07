4ffa647a37b42d0c9044dae47fcd530a8baeb30b
yolandagb <yolandagarciabarranco@hotmail.com>
2021-05-07 20:02:14 +0000

    primera parte hecha

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Current
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,14 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
-    return jsonify(response_body), 200
+@app.route('/current', methods=['GET'])
+def get_all_current():
+    all_current = Current.query.all()
+    currents_serialized = []
+    for current in all_current: 
+        print(current.serialize)
+        currents_serialized.append = ("current.serialize")
+    return jsonify(currents_serialized), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,40 @@
+# Hacer una tabla de la "familia general"
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Current(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+    parents= db.Column(db.Integer, db.ForeignKey('parent.id'),
+        nullable=False)
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Current %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name,
             # do not serialize the password, its a security breach
         }
+
+class Parent(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    children = db.relationship('Current',backref='parent', lazy=True) 
+
+    def __repr__(self):
+        return '<Parent %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+         
