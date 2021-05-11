f68bb7adc175c9fbcba01579cb947848a5a39838
yolandagb <yolandagarciabarranco@hotmail.com>
2021-05-11 10:05:07 +0000

    Ultimos cambios, revisar

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, Current
-#from models import Person
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -21,25 +21,52 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
 
 @app.route('/current', methods=['GET'])
 def get_all_current():
     all_current = Current.query.all()
     currents_serialized = []
-    for current in all_current: 
+    for current in all_current:
         print(current.serialize)
         currents_serialized.append = ("current.serialize")
     return jsonify(currents_serialized), 200
 
-# this only runs if `$ python src/main.py` is executed
+
+@app.route('/parent', methods=['GET'])
+def get_all_current():
+    all_parent = Parent.query.all()
+    parents_serialized = []
+    for parent in all_parent:
+        print(parent.serialize)
+        parents_serialized.append = ("parent.serialize")
+    return jsonify(currents_serialized), 200
+
+
+@app.route('/grandParent', methods=['GET'])
+ def get_all_current():
+    all_grandParent = GrandParent.query.all()
+    GrandParents = []
+    for Parent in all_GrandParent:
+         print(GrandParents.serialize())
+        GrandParents.append(GrandParent.serialize)
+
+    return jsonify(GrandParents), 200   
+
+
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
